95f24eaa027f853ece1b519ffe8b71c0af2e4853
botmakin <119750664+botmakin@users.noreply.github.com>
2022-12-21 14:39:59 +0530

    Update force_sub_handler.py

--- core/handlers/force_sub_handler.py
+++ core/handlers/force_sub_handler.py
@@ -1,4 +1,4 @@
-# (c) @AbirHasan2005
+
 
 import asyncio
 from configs import Config
@@ -12,7 +12,7 @@ async def handle_force_subscribe(bot, cmd):
         if user.status == "kicked":
             await bot.send_message(
                 chat_id=cmd.from_user.id,
-                text="Sorry Sir, You are Banned to use me. Contact my [Support Group](https://t.me/linux_repo).",
+                text="Sorry Sir, You are Banned to use me. Contact my [Support Group](https://t.me/movies_halt_owner_bot).",
                 parse_mode="markdown",
                 disable_web_page_preview=True
             )
@@ -42,7 +42,7 @@ async def handle_force_subscribe(bot, cmd):
     except Exception:
         await bot.send_message(
             chat_id=cmd.from_user.id,
-            text="Something went Wrong. Contact my [Support Group](https://t.me/linux_repo).",
+            text="Something went Wrong. Contact my [Support Group](https://t.me/movies_halt_owner_bot).",
             parse_mode="markdown",
             disable_web_page_preview=True
         )
